9059265f820e5dc378907d00bd0938cb2c3b7bcf
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-30 16:45:42 +0100

    salvataggio

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const lista = document.getElementById("lista")
 
 
-for (let i = 0; i < 101; i++) {
+for (let i = 1; i < 101; i++) {
     
-    lista.innerHTML = lista.innerHTML + `<p>Numero: ${i}</p>`;
     
-    if (i%3==0) {
-       
-        lista.innerHTML = lista.innerHTML + `<p>Numero: ${"fizz"}</p>`;
+    if (i%15==0) {
+        
+        lista.innerHTML = lista.innerHTML + `<p>Numero: ${"fizzBuzz"}</p>`;
         
     } else if (i%5==0) {
-       
+        
         lista.innerHTML = lista.innerHTML + `<p>Numero: ${"buzz"}</p>`;
         
+    } else if (i%3==0) {
+        
+        lista.innerHTML = lista.innerHTML + `<p>Numero: ${"Fizz"}</p>`;
+        
+    } 
+    else {
+        
+        lista.innerHTML = lista.innerHTML + `<p>Numero: ${i}</p>`;
+
     }
     
 }
